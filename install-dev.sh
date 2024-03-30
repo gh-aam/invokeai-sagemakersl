efa18b79bf3d7e1b5edf591ec7a7d1c52cc0aa0f
Abdullah Al Mamun <m98.aam@gmail.com>
2024-03-31 04:47:52 +0600

    Update install-dev.sh

--- install-dev.sh
+++ install-dev.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
+
 dir=$(pwd)
-conda create -y -n  invoke python=3.10
+
+conda create -y -n invokeai python=3.11
+
 eval "$(conda shell.bash hook)"
-conda activate invoke
-mkdir -p /tmp/invoke/models
+conda activate invokeai
+
+mkdir -p /tmp/invokeai/models
 mkdir -p ~/invokeai
 if ! test -e ~/invokeai/models
 then
-  ln -s /tmp/invoke/models ~/invokeai/
+    ln -s /tmp/invokeai/models ~/invokeai/
 fi
-python --version
+
 cd ~
-git clone --depth 1 --branch v3.4.0rc1 https://github.com/invoke-ai/InvokeAI
+git clone --depth 1 --branch v4.0.0rc6 https://github.com/invoke-ai/InvokeAI
+
 cd InvokeAI
-pip install -e .[xformers] --use-pep517 --extra-index-url https://download.pytorch.org/whl/cu118
+pip install -e .[xformers] --use-pep517 --extra-index-url https://download.pytorch.org/whl/cu121
+
 cd $dir
 pip install pyngrok
 pip install opencv-python-headless
 python patch-16bit-model-download.py
 conda install glib -y
+
 conda deactivate
