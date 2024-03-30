50324a31eddb5c4b89b2543ec701399fbb49ddd9
Abdullah Al Mamun <m98.aam@gmail.com>
2024-03-31 04:51:19 +0600

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+
 eval "$(conda shell.bash hook)"
-conda activate invoke
-mkdir -p /tmp/invoke/models
+conda activate invokeai
+
+mkdir -p /tmp/invokeai/models
 mkdir -p ~/invokeai
 if ! test -e ~/invokeai/models
 then
-  ln -s /tmp/invoke/models ~/invokeai/
+    ln -s /tmp/invokeai/models ~/invokeai/
 fi
 
-invokeai-configure --root_dir ~/invokeai --yes  --default_only --skip-sd-weights --no-full-precision
+invokeai-configure --root_dir ~/invokeai --yes --default_only --skip-sd-weights --no-full-precision
 
 rm -rf ~/.cache/huggingface/hub/*
