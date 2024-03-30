8e0cabe94f5429acaf0791ee8dd0ee11180bd421
Abdullah Al Mamun <m98.aam@gmail.com>
2024-03-31 05:19:06 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+
 eval "$(conda shell.bash hook)"
-conda activate invoke
-mkdir -p /tmp/invoke/models
+conda activate invokeai
+
+mkdir -p /tmp/invokeai/models
 if [ ! -L ~/invokeai/models ] 
 then
-  ln -s /tmp/invoke/models ~/invokeai/
+    ln -s /tmp/invokeai/models ~/invokeai/
 fi
 
 if [ $# -eq 0 ]
 then
-  python start-ngrok.py  
+    python start-ngrok.py  
 elif [ $1 = "reset" ]
 then
-  python start-ngrok.py --reset 
+    python start-ngrok.py --reset 
 fi
