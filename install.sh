52e7b5b67233ec0b151988115ca6b769741754d0
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-05 19:28:08 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conda create -y -n invokeai python=3.11 glib
+conda create -y -n invokeai python=3.11
 
 eval "$(conda shell.bash hook)"
 conda activate invokeai
@@ -13,7 +13,8 @@ then
 fi
 
 pip install "InvokeAI[xformers]" --use-pep517 --extra-index-url https://download.pytorch.org/whl/cu121
-pip install pyngrok
 python patch-16bit-model-download.py
+pip install pyngrok
+conda install -y glib
 
 conda deactivate
