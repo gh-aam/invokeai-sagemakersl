7a4034575c51c8ca2f143817e6b2f1a5c261e8a1
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-12 23:20:32 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 export INVOKEAI_ROOT=~/invokeai
-
 mkdir $INVOKEAI_ROOT
-cd $INVOKEAI_ROOT
 
+cd $INVOKEAI_ROOT
 conda create -p .venv python=3.11 glib
+
 eval "$(conda shell.bash hook)"
 conda activate $INVOKEAI_ROOT/.venv
 
