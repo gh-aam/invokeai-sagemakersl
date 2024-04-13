df079b34a51b1f2acf7d29010bf1bd554b05f329
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-13 07:53:21 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 mkdir -p ~/invokeai
-
 cd ~/invokeai
-conda create -p .venv python=3.11 glib
 
+conda create -p .venv python=3.11 glib
 eval "$(conda shell.bash hook)"
 conda activate ~/invokeai/.venv
+
 python3 -m pip install --upgrade pip
 pip install InvokeAI --use-pep517
 pip install pyngrok
+
 conda deactivate
